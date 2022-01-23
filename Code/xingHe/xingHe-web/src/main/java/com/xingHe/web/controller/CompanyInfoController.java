42545ab730182a1c5aa2d4d9b1628a39e3b9cf86
liyanbo <liyanbofly@sina.com>
2022-01-23 21:14:37 +0800

    删除 target 目录

--- Code/xingHe/xingHe-web/src/main/java/com/xingHe/web/controller/CompanyInfoController.java
+++ Code/xingHe/xingHe-web/src/main/java/com/xingHe/web/controller/CompanyInfoController.java
@@ -3,6 +3,8 @@ package com.xingHe.web.controller;
 import org.apache.commons.lang.ClassUtils;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.applet.AppletContext;
+
 @RestController
 public class CompanyInfoController {
 
